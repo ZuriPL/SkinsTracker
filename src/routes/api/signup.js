bb270871bdd20304644c2924171f676c772663a8
ZuriPL <michal.bak222@gmail.com>
2022-06-12 20:57:54 +0200

    changed behaviour of logout to not delete cookies

---
+++ src/routes/api/signup.js
@@ -0,0 +1,41 @@
+import clientPromise from '$lib/mongodb-client'
+import { ObjectId } from 'mongodb'
+import { serialize } from 'cookie'
+import { v4 as uuidv4 } from 'uuid'
+
+export async function post({ request }) {
+	const dbConnection = await clientPromise
+	const db = dbConnection.db('SkinsTracker')
+	const collection = db.collection('SkinsTracker')
+
+	const data = await collection.findOne({ _id: ObjectId('62923f1b36006ed1642ff7d5') })
+
+	let body = await request.json()
+
+	let user = data.users.filter((user) => user.email == body.login)[0]
+
+	if (user != undefined) {
+		return {
+			status: 200,
+			body: { error: 'user already exists' },
+		}
+	}
+
+	let newUser = {
+		email: body.login,
+		password: body.password,
+		skins: [],
+	}
+
+	await collection.updateOne(
+		{ _id: ObjectId('62923f1b36006ed1642ff7d5') },
+		{
+			$push: { users: newUser },
+		}
+	)
+
+	return {
+		status: 200,
+		body: { msg: 'Succesfully created a user' },
+	}
+}
